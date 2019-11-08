a3f06c98d2949b9595546e33ab31436245b0d3e2
Khoa Nguyen <henrykhoanguyen@gmail.com>
2019-11-08 11:23:22 -0800

    working on user authorization

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const path = require('path');
-const fileupload = require('express-fileupload');
+const path = require("path");
+const fileupload = require("express-fileupload");
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require('morgan');
-const colors = require('colors');
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/error');
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -16,6 +16,7 @@ connectDB();
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
@@ -23,19 +24,20 @@ const app = express();
 app.use(express.json());
 
 // Dev logging middleware
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // File uploading
 app.use(fileupload());
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
@@ -43,11 +45,13 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // Handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
+process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`.red);
   // Close server and exit process
   server.close(() => process.exit(1));
