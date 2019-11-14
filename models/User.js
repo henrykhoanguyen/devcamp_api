ac9f3b6c6160fcb58a19cb6c0158477478680236
Khoa Nguyen <henrykhoanguyen@gmail.com>
2019-11-14 13:37:28 -0800

    currently working on forget password, sending email

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
+const crypto = require("crypto");
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs'); // to encrypt passwords
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs"); // to encrypt passwords
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -19,10 +20,10 @@ const UserSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ["user", "publisher"],
-    default: 'user'
+    default: "user"
   },
   password: {
-    type: String, 
+    type: String,
     required: [true, "Please enter a password"],
     minlength: 6,
     select: false
@@ -35,23 +36,49 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-// Encrypt password using bcrypt 
-UserSchema.pre('save', async function(next) {
+// Encrypt password using bcrypt
+UserSchema.pre("save", async function(next) {
+
+  // if password is not changed then skip the hashing of password
+  // we need this because we're only saving a user in forgotPassword function 
+  // in auth.js controller, but we are not adding a password.
+  if(!this.isModified('password')){
+    next();
+  }
+
+  // this only run when password is modified. ex: in register user.
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-
 });
 
 // Sign JWT and return
-UserSchema.methods.getSignedJwtToken = function () {
+UserSchema.methods.getSignedJwtToken = function() {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRE
   });
-}
+};
 
 // Match user entered password to hashed password in database
 UserSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
-}
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function() {
+  // Generate a token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hash token and set resetPasswordToken field
+  // more can be finded in node.crypto doc
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+  
+  return resetToken;
+};
 
 module.exports = mongoose.model("User", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,18 @@
-const express = require ('express');
-const { register, login, getMe } = require('../controllers/auth');
+const express = require("express");
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword
+} = require("../controllers/auth");
 
-const { protect } = require('../middleware/auth');
+const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.get('/me', protect, getMe);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/me", protect, getMe);
+router.post("/forgotpassword", forgotPassword);
 
-module.exports = router; 
+module.exports = router;
