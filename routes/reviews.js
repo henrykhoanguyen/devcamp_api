420c9d9cb7b77842e3835abca183eaf06f4420a0
Khoa Nguyen <henrykhoanguyen@gmail.com>
2019-11-22 14:48:54 -0800

    finished implement get request to get all reviews, a singular review, and all reviews of a bootcamp

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,8 @@
 const express = require("express");
 
 const {
-  getReviews
+  getReviews,
+  getReview
 } = require("../controllers/reviews");
 
 const Review = require("../models/Review");
@@ -20,5 +21,7 @@ router
     }),
     getReviews
   );
+
+router.route('/:id').get(getReview);
 
 module.exports = router;
--- seeder.js
+++ seeder.js
@@ -8,8 +8,9 @@ dotenv.config({ path: "./config/config.env" });
 
 // Load models
 const Bootcamp = require("./models/Bootcamp");
-const Course = require('./models/Course');
-const User = require('./models/User');
+const Course = require("./models/Course");
+const User = require("./models/User");
+const Review = require("./models/Review");
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -32,18 +33,23 @@ const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
 );
 
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8")
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
-    console.log('Data imported...'.green.inverse);
+    await Review.create(reviews);
+    console.log("Data imported...".green.inverse);
     process.exit();
-  } catch (err){
+  } catch (err) {
     console.error(err);
   }
-}
+};
 
 // Delete data
 const deleteData = async () => {
@@ -51,15 +57,16 @@ const deleteData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
-    console.log('Data destroyed...'.red.inverse);
+    await Review.deleteMany();
+    console.log("Data destroyed...".red.inverse);
     process.exit();
-  } catch (err){
+  } catch (err) {
     console.error(err);
   }
-}
+};
 
-if(process.argv[2] === '-i'){
+if (process.argv[2] === "-i") {
   importData();
-} else if (process.argv[2] === '-d'){
+} else if (process.argv[2] === "-d") {
   deleteData();
 }
