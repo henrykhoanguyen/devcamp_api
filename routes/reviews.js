ab24e16f8df692e4a8c721e706eb8f4c09a233b7
Khoa Nguyen <henrykhoanguyen@gmail.com>
2019-11-22 16:49:49 -0800

    finished with review controllers and routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,10 @@ const express = require("express");
 
 const {
   getReviews,
-  getReview
+  getReview,
+  addReview,
+  updateReview,
+  deleteReview
 } = require("../controllers/reviews");
 
 const Review = require("../models/Review");
@@ -20,8 +23,12 @@ router
       select: "name description"
     }),
     getReviews
-  );
+  )
+  .post(protect, authorize("user", "admin"), addReview);
 
-router.route('/:id').get(getReview);
+router
+  .route("/:id")
+  .get(getReview)
+  .put(protect, authorize("user", "admin"), updateReview).delete(protect, authorize("user", "admin"), deleteReview);
 
 module.exports = router;
